05ec20aab539c16761574eb5103e224e40daf30f
Abhinav Kumar <abhi2407singh@gmail.com>
2024-02-11 14:30:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,124 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect, useRef } from 'react';
+import axios from 'axios';
+
+
 
 function App() {
+  const [name, setName]= useState('');
+ const [email, setEmail]= useState('');
+const [password, setPassword]= useState('');
+
+  const containerRef = useRef(null);
+
+  const handleRegisterClick = () => {
+    containerRef.current.classList.add("active");
+  };
+
+  const handleLoginClick = () => {
+    containerRef.current.classList.remove("active");
+  };
+
+  async function registerUser(ev){
+    ev.preventDefault();
+    try{
+      await axios.post('/register',{
+    name,
+    email,
+    password,
+   });
+
+   alert('User registered successfully. Now you can login.');   
+}catch(e){
+  alert('Registration failed. Please try again later.');  
+  
+   
+}}
+async function handleLoginSubmit(ev) {
+  ev.preventDefault();
+
+  try {
+      const { data } = await axios.post("/login", { email, password });
+      alert("Login successful");
+
+  } catch (e) {
+      alert("Login failed. Please try again later.");
+  }
+}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container" id="container" ref={containerRef}>
+        <div className="form-container sign-up">
+            <form onSubmit={registerUser}>
+                <h1>Create Account</h1>
+                <div className="social-icons">
+                    <a href="#" className="icon"><i className="fa-brands fa-google" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-facebook" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-linkedin" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-github" style={{ color: '#000000' }}></i></a>
+                </div>
+                <span>or use your email for registration</span>
+                <input type="name" 
+                placeholder="Name"
+                value={name}
+                onChange={ev => setName(ev.target.value)}
+                />
+                <input type="email" 
+                value={email}
+                onChange={ev => setEmail(ev.target.value)}
+                placeholder="Email"/>
+                <input type="password" 
+                value={password}
+                onChange={ev => setPassword(ev.target.value)}
+                placeholder="Password"/>
+                <button>Sign up</button>
+            </form>
+        </div> 
+        <div className="form-container sign-in">
+            <form onSubmit={handleLoginSubmit}>
+                <h1>Sign In</h1>
+                <div className="social-icons">
+                    <a href="#" className="icon"><i className="fa-brands fa-google" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-facebook" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-linkedin" style={{ color: '#000000' }}></i></a>
+                    <a href="#" className="icon"><i className="fa-brands fa-github" style={{ color: '#000000' }}></i></a>
+                </div>
+                <span>or use your email password</span>
+              
+                <input type="email" 
+                value={email}
+                onChange={(ev) => setEmail(ev.target.value)}
+                placeholder="Email"/>
+                <input type="password" 
+                value={password}
+                onChange={(ev) => setPassword(ev.target.value)}
+                placeholder="Password"/>
+                <a href="#">Forgot your password?</a>
+                <button>Sign In</button>
+            </form>
+        </div> 
+        <div className="toggle-container">
+            <div className="toggle">
+                <div className="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button className="hidden" id="login" onClick={handleLoginClick}>Sign In</button>
+                </div>
+                <div className="toggle-panel toggle-right">
+                    <h1>Hello friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button className="hidden" id="register" onClick={handleRegisterClick}>Sign Up</button>
+                </div>
+            </div>
+        </div>
     </div>
+    
+    </>
+
   );
+
 }
 
 export default App;
